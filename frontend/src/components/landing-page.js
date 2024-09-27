36671e7ccdaaa53604235a42183af65fd924074d
fredericstrand <strandfrederic@outlook.com>
2024-09-27 18:46:11 +0200

    getting somehwere :)

--- frontend/src/components/landing-page.js
+++ frontend/src/components/landing-page.js
@@ -3,13 +3,35 @@ import "../styles/landing-page.css"
 
 function Landing_page() {
     return (
+        <>
         <div class="hero-container">
             <div class="hero-content">
-                <h1>Welcome to a community of dedicated FPL players.</h1>
-                <p>Through our dashboard and machine learning applications we help you along the way to victory in your FPL League</p>
-                <a href="login">Sign up now!</a>
+                <h1>Welcome to a Community of Dedicated FPL Players.</h1>
+                <p>Through our dashboard and machine learning applications we will help you along the way to victory in your FPL League</p>
+                <a href="login"><span>SIGN UP NOW </span></a>
+            </div>
+            <div class="hero-content-image">
+                <img src="/images/intro-image.png" alt="into image" id="intro-image"></img>
             </div>
         </div>
+
+        <div class="intro-container">
+            <div class="intro-content">
+                <div class ="intro-content-text-1">
+                    <h2>We offer industry leading services</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="intro-content-text-2">
+                    <h2>We offer industry leading services</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="intro-content-text-3">
+                    <h2>We offer industry leading services</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+        </>
     );
 }
 
